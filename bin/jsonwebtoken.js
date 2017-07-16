db6ea516966794d8d8c0e2a84459fdd293eb30b9
Andre Schmitt <andre1502@gmx.de>
2017-07-16 13:10:28 +0200

    add new projekt

---
+++ bin/jsonwebtoken.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Andre on 16.07.2017.
+ */
+var jwt = require('jsonwebtoken');
+var secret = '42isnice!TEXTVENTURER'
+exports.generateToken = function(userName)
+{
+    //generate token
+    var token = jwt.sign({exp: Math.floor(Date.now() / 1000) + 60*180,
+        username: userName
+    },secret);
+    return token
+};
+
+exports.checkToken = function(req, res, next) {
+
+};
---
+++ routes/blog.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('./../bin/jsonwebtoken.js');
+bloglist= require('../ressorces/blog.json');
+
+/* GET home page. */
+router.get('/', getBlogLIst);
+router.route('/:id')
+        .get(getPost_Id)
+        .delete(delPost_Id);
+
+function getBlogLIst(req, res){
+
+    res.json(bloglist);
+}
+
+function getPost_Id (req, res) {
+    if (bloglist[req.params.id].hidden) {
+        res.status(401).json({
+            message: 'Forbidden'
+        });
+        return;
+    }
+    res.json(bloglist[req.params.id]);
+}
+
+function  delPost_Id(req, res) {
+    var id = req.params.id;
+    delete bloglist[id];
+    res.status(200).json({
+        message: "Deleted :id"
+    })
+}
+
+
+module.exports = router;
+//authorization
