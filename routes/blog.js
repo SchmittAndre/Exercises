505b15ce837507b607692016ecb836a6776a8d75
SchmittAndre <andre1502@gmx.de>
2017-07-25 18:33:43 +0200

    added delete post funktion

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 var jwt = require('./../bin/jsonwebtoken.js');
 bloglist= require('../ressorces/blog.json');
 
 /* GET home page. */
 router.get('/', getBlogLIst);
-router.route('/:id')
+router.route('/:id([0-9]+)')
         .get(getPost_Id)
         .delete(delPost_Id);
 
@@ -25,12 +26,32 @@ function getPost_Id (req, res) {
 
 function  delPost_Id(req, res) {
     var id = req.params.id;
+    if (bloglist[req.params.id].hidden) {
+        res.status(401).json({
+            message: 'Forbidden'
+        });
+        return;
+    }
     delete bloglist[id];
+
+    fs.writeFile("./ressorces/blog.json", JSON.stringify(bloglist), function(err) {
+        if(err) {
+            res.status(500).json({
+                message: 'System Error'
+            })
+        }
+        else{
+                res.status(200).json({
+                    message: 'Post Deleted'
+                });
+                return
+        }
+    });
+
     res.status(200).json({
         message: "Deleted :id"
     })
 }
-
 
 module.exports = router;
 //authorization
