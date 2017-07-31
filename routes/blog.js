07a4916f8aa942b5d0c82ccd969661757a94d102
Simon Vollmer <simonvollmer91@gmail.com>
2017-07-31 17:51:06 +0200

    Create function added

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
 //var jwt = require('./../bin/jsonwebtoken.js');
 bloglist= require('../ressorces/blog.json');
 
 /* GET home page. */
 router.get('/', getBlogLIst);
+router.post('/', createPost);
 router.route('/:id([0-9]+)')
         .get(getPost_Id)
         .delete(delPost_Id)
@@ -43,7 +45,7 @@ function  delPost_Id(req, res) {
     }
     delete bloglist[id];
 
-    fs.writeFile("./ressorces/blog.json", JSON.stringify(bloglist), function(err) {
+    fs.writeFile("./ressorces/blog.json", JSON.stringify(bloglist,null,3), function(err) {
         if(err) {
             res.status(500).json({
                 message: 'System Error'
@@ -77,7 +79,7 @@ function putPost_IP(req, res) {
     bloglist[id].hidden = req.body.hidden;
     bloglist[id].tags = req.body.tags;
 
-    fs.writeFile("./ressorces/blog.json", JSON.stringify(bloglist), function(err) {
+    fs.writeFile("./ressorces/blog.json", JSON.stringify(bloglist,null,3), function(err) {
         if(err) {
             res.status(500).json({
                 message: 'System Error'
@@ -93,6 +95,53 @@ function putPost_IP(req, res) {
     res.status(200).json(bloglist[id])
 
 }
+
+function createPost(req, res)   {
+    if (res.locals.authentorized === false) {
+        res.status(401).json({
+            message: 'Forbidden'
+        });
+        return;
+    }
+
+    if (!req.body.title || !req.body.picture || !req.body.author || !req.body.about || !req.body.released || !req.body.hidden || !req.body.tags) {
+        res.status(400).send();
+        return;
+    }
+
+    var newPostIndex = bloglist[bloglist.length-1].index+1;
+
+
+    var newPost = {
+    _id     : new ObjectID(),
+    index   : newPostIndex,
+    title   : req.body.title,
+    picture : req.body.picture,
+    author  : req.body.author,
+    about   : req.body.about,
+    released: req.body.released,
+    hidden  : req.body.hidden,
+    tags    : req.body.tags
+    };
+
+    bloglist.push(newPost);
+
+    fs.writeFile("./ressorces/blog.json", JSON.stringify(bloglist,null,3), function(err) {
+        if(err) {
+            res.status(500).json({
+                message: 'System Error'
+            })
+        }
+        else{
+            res.status(201).json({
+                message: 'Post Created'
+            });
+        }
+    });
+
+
+}
+
 
 module.exports = router;
 //authorization
